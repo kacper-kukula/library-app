9c5db2fe5524775dde9fae99ab74f26bf4399054
Kacper <kacperkukula97@gmail.com>
2024-07-02 00:52:29 +0200

    Removed unused imports

--- src/main/java/com/libraryapp/service/impl/BookServiceImpl.java
+++ src/main/java/com/libraryapp/service/impl/BookServiceImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
