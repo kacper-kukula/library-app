ca8f7b0aede08d8b333947d16dff30f2a29e714e
Kacper <kacperkukula97@gmail.com>
2024-07-02 21:01:31 +0200

    Fixed GitHub CI test db connection issue - implemented testcontainers

--- src/test/java/com/libraryapp/LibraryAppApplicationTests.java
+++ src/test/java/com/libraryapp/LibraryAppApplicationTests.java
@@ -1,13 +1,30 @@
 package com.libraryapp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MongoDBContainer;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class LibraryAppApplicationTests {
+
+    private static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest")
+            .withExposedPorts(27017);
+
+    @BeforeEach
+    void setUp() {
+        mongoDBContainer.start();
+        System.setProperty("spring.data.mongodb.uri", mongoDBContainer.getReplicaSetUrl());
+    }
 
     @Test
     void contextLoads() {
+        assertThat(mongoDBContainer.isRunning()).isTrue();
     }
 
 }
